e1a7c3d8f3fa06d645963ba67ec2ee9acd54ae8f
Bipin <bipinkr7742@gmail.com>
2024-05-23 15:55:21 +0530

    set cors origin *

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -6,7 +6,11 @@ const app = express();
 
 const server = http.createServer(app)
 
-const io = new Server(server);
+const io = new Server(server, {
+    cors: {
+        origin: '*'
+    }
+});
 
 const getReceiverSocketId = (receiverId) => {
     return userSocketMap[receiverId]
